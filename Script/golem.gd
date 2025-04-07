575b5c4160e19b125eb46a112e09e4a0bf27a6dc
Mgodar42 <mgodar@algomau.ca>
2025-04-07 15:17:16 -0400

    hit detection
    wanted to make golem take damage from player first ,just made hit detection area will complete after making player

--- Script/golem.gd
+++ Script/golem.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 
 const speed = 60
-
+var health = 3
+var is_alive = true
 var direction =1
 
 @onready var ray_castright: RayCast2D = $RayCastright
 @onready var ray_castleft: RayCast2D = $RayCastleft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var area_2d: Area2D = $Area2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +22,7 @@ func _process(delta: float) -> void:
 		animated_sprite.flip_h = false
 	position.x += delta*speed*direction
 	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
